0fa7d7fcbb1f2c7d78e37f42195154b5f97a4e25
list <johndoe@example.com>
2024-08-28 09:38:36 +0530

    modified

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const tBtn = document.querySelector("#buttonImg");
 const nav = document.querySelector(".pcNav");
+const links = document.querySelectorAll("li");
+tBtn.addEventListener('click', toggleNavbar);
 
-tBtn.addEventListener('click',toggleNavbar);
-
-function toggleNavbar(){
+function toggleNavbar() {
     nav.classList.toggle("hidden");
 }
+
+
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        nav.classList.toggle('hidden');
+    })
+});
